d4d088d402ff39320dbea9be69df0c6f2c93489e
steffzbl <50582040+steffzbl@users.noreply.github.com>
2023-12-05 12:10:12 +0100

    add haierac
    Add HaierAC

---
+++ components/haier_ac/haier_ac.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "esphome/core/log.h"
+#include "esphome/components/climate/climate.h"
+#include "esphome/components/sensor/sensor.h"
+
+#include "IRremoteESP8266.h"
+#include "IRsend.h"
+#include "ir_Haier.h"
+
+
+namespace esphome {
+namespace haier_ac {
+
+const uint8_t HAIER_AC_TEMP_MIN = 16;  // 16C
+const uint8_t HAIER_AC_TEMP_MAX = 30;  // 32C
+
+class HaierClimate : public climate::Climate {
+ public:
+  HaierClimate() : climate::Climate() {}
+ public:
+  void set_sensor(sensor::Sensor *sensor);
+  void init(sensor::Sensor *sensor, uint16_t pin);
+ 
+ protected:
+  IRHaierAC *ac_{nullptr};
+  sensor::Sensor *sensor_{nullptr};
+  
+  void setup_ir_cmd();
+
+  climate::ClimateTraits traits() override;
+  void control(const climate::ClimateCall &call) override;
+
+};
+
+}  // namespace haier_ac
+}  // namespace esphome
